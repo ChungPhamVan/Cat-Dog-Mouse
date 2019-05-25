6915adaeffa128561f2ccbe35265ad25dfbcd19d
ChungPhamVan <phamvanchung11111999@gmail.com>
2019-05-25 08:36:36 +0700

    inprove Cat Animal

--- Cat.js
+++ Cat.js
@@ -1,5 +1,15 @@
+var Dog = require('./Dog.js');
+
 function Cat(name, age) {
 	this.name = name;
 	this.age = age;
+	this.stomach = [];
 }
+Cat.prototype.eat = function(animal) {
+	if(animal instanceof Dog) {
+		this.stomach.push(animal);
+	}else {
+		throw new ('Cat only eat Dog!');
+	}
+};
 module.exports = Cat;
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
+var Horse = require('./Horse.js');
 
 var Micky = new Dog('Micky', 2);
 var Tom = new Cat('Tom', 1.5);
+var Huhu = new Horse();
+try{
+	Tom.eat(Huhu);
+}catch {
+	console.log('Error: Tom only eat Micky');
+}
+console.log(Tom.eat(Micky));
 
-console.log('Hello World');
-console.log(Micky.eat(Tom));
